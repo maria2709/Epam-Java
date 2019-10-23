0d022727e8c1119c590660f61d874c07a71941d4
maria2709 <37977707+maria2709@users.noreply.github.com>
2019-10-23 16:59:14 +0300

    Create Main.java

---
+++ Homework_lesson3/Task2_3/Main.java
@@ -0,0 +1,34 @@
+package fasttrack_task2_3;
+
+public class Main {
+	public static void main(String[] args) {
+		int line = 5;
+		int i = 0;
+		int j = 0;
+		do {
+			while (j < line) {	
+				System.out.print("  ");
+				j++;
+			}
+
+			do {
+				System.out.print("* ");
+				i++;
+			}
+			
+			while (i <= 5 - line);
+			
+			i = 0;
+			j = 0;
+			line--;
+			System.out.println();
+		}
+		
+		while (line >= 1);
+							
+	}
+
+
+}
+
+
