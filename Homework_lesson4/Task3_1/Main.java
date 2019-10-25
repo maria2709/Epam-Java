5d78bb389933c7b001652ca4b56f1138bb096b42
maria2709 <37977707+maria2709@users.noreply.github.com>
2019-10-25 12:09:15 +0300

    Update Main.java

--- Homework_lesson4/Task3_1/Main.java
+++ Homework_lesson4/Task3_1/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your text: ");
         String text = sc.nextLine();
         System.out.println(getUniq(text));
     }
